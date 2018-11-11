e4a56d52a1f09eb952cd1bd7029bc2c71752a770
Ben Don <benbdon@gmail.com>
2018-11-11 14:03:40 -0600

    simplified the node

--- basics/src/topic_publisher.py
+++ basics/src/topic_publisher.py
@@ -3,16 +3,13 @@
 import rospy #required for every ROS Python node
 from std_msgs.msg import Int32 # define message type
 
-rospy.init_node('topic_publisher') #initialize node
-pub = rospy.Publisher('counter', Int32, queue_size = 10) #publish to a topic called counter
+rospy.init_node('topic_publisher') # initialize node with the name topic_publisher
+pub = rospy.Publisher('counter', Int32) #, queue_size = 10) # publish to a topic called counter
 
-rate = rospy.Rate(2) #publish at 2 Hz (twice per second)
+rate = rospy.Rate(2) # set a 2 Hz update rate on this node
 
 count = 0
-while not rospy.is_shutdown(): #run until shutdown
-    pub.publish(count) # publish current value of counter
-    count += 1 # increment counter
-    rate.sleep() # sleep long enough to ensure this loop happens at 2 Hz
-
-
-#test 
+while not rospy.is_shutdown(): # run until shutdown
+    pub.publish(count) 
+    count += 1
+    rate.sleep() 
