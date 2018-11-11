7b4719ed4b0f17df415daf22bf835bbabcdd7600
Ben Don <benbdon@gmail.com>
2018-11-11 15:17:28 -0600

    creating a custom message type

--- basics/src/topic_publisher.py
+++ basics/src/topic_publisher.py
@@ -5,7 +5,7 @@ from std_msgs.msg import Int32 # define message type
 
 rospy.init_node('topic_publisher') # initialize node with the name topic_publisher
 
-pub = rospy.Publisher('counter', Int32) #, queue_size = 10) # publish to a topic called counter
+pub = rospy.Publisher('counter', Int32, queue_size = 10, latch = True) # publish to a topic called counter
 
 rate = rospy.Rate(2) # set a 2 Hz update rate on this node
 
