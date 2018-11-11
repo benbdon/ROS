1e48abfca6d983cf787fb42bbb746a0b47058549
Ben Don <benbdon@gmail.com>
2018-11-11 14:30:58 -0600

    Removed comment

--- basics/src/topic_subscriber.py
+++ basics/src/topic_subscriber.py
@@ -11,5 +11,3 @@ rospy.init_node('topic_subscriber') # initialize node
 sub = rospy.Subscriber('counter', Int32, callback) # call callback function every time you get a new message on topic 'counter'
 
 rospy.spin() # give control to ROS - only return when thenode is ready to shutdown (shorthand for a top-level while loop)
-
-#test
