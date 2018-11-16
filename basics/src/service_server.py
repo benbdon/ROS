012cae00e118cf68d57871ab010f48954369a070
Ben Don <benbdon@gmail.com>
2018-11-16 08:44:39 -0600

    documented code functionality

--- basics/src/service_server.py
+++ basics/src/service_server.py
@@ -2,13 +2,15 @@
 
 import rospy
 
-from basics.srv import WordCount,WordCountResponse
+from basics.srv import WordCount,WordCountResponse # since this is the node that hosts the service, 
+                                                # seems likely that a node which uses this would
+                                                # import WordCount and WordCountRequest
 
-def count_words(request):
+def count_words(request): #call back function that counts words in a string
     return WordCountResponse(len(request.words.split()))
 
-rospy.init_node('service_server')
+rospy.init_node('service_server') #initialize node
 
-service = rospy.Service('word_count', WordCount, count_words)
+service = rospy.Service('word_count', WordCount, count_words) # advertise the service
 
-rospy.spin()
+rospy.spin() #keep node alive (watching for callbacks) until shutdown
