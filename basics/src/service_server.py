f82272190e4d28675137cea8e18a9661c64a8753
Ben Don <benbdon@gmail.com>
2018-11-16 14:58:51 -0600

    Improved detailed documentation

--- basics/src/service_server.py
+++ basics/src/service_server.py
@@ -7,10 +7,13 @@ from basics.srv import WordCount,WordCountResponse # since this is the node that
                                                 # import WordCount and WordCountRequest
 
 def count_words(request): #call back function that counts words in a string
-    return WordCountResponse(len(request.words.split()))
+    return WordCountResponse(len(request.words.split())) #takes parameters that match the service-definition
+                                                        #unsigned int
 
 rospy.init_node('service_server') #initialize node
 
-service = rospy.Service('word_count', WordCount, count_words) # advertise the service
+service = rospy.Service('word_count', WordCount, count_words) # advertise the service (assign a name for the
+                                                            # service, map it to a service-defintion file of I/O 
+                                                            # names and types, callback function name)
 
 rospy.spin() #keep node alive (watching for callbacks) until shutdown
