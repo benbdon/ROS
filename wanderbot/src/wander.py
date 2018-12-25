938eb4ede3a6484140c09b6defe1ca8f501f5245
Ben Don <benbdon@gmail.com>
2018-12-25 11:07:12 -0600

    overhaul the existing wander.py code (mostly removing the OOP nature)

--- wanderbot/src/wander.py
+++ wanderbot/src/wander.py
@@ -1,48 +1,38 @@
 #!/usr/bin/env python
 import rospy
 from geometry_msgs.msg import Twist
-from sensor_msgs.msg import LaserScan #Import the LaserScan message since we'll be using it
+from sensor_msgs.msg import LaserScan
 
-class Wandering(object):
-    def __init__(self):
-        ''' Initialize parameters'''
-        rospy.init_node('wander')
-        self.cmd_vel_pub = rospy.Publisher('cmd_vel',Twist,queue_size=1)
-        self.twist = Twist()
-        self.state_change_time = rospy.Time.now()  #These are the two variables we'll use for our controller logic, driving state and time
-        self.driving_forward = True #These are the two variables we'll use for our controller logic, driving state and time
-        self.rate = rospy.Rate(10)
+def scan_callback(msg):
+    global g_range_ahead
+    g_range_ahead = min(msg.ranges) #This stores the minimum range our simulated laser scanner detects. 
+                            # We could decide to use the middle element of the ranges array instead with 
+                            # range_ahead = msg.ranges[len(msg.ranges)/2]. Alternatively we could use 
+                            # msg.range_min to use the minimum value the laser scanner is capable of sensing 
+                            # (assuming our driver has properly set this). How would these choices affect the TurtleBot's behavior.
 
-    def scan_callback(self,msg):
-        range_ahead = min(msg.ranges) #This stores the minimum range our simulated laser scanner detects. We could decide to use the middle element of the ranges array instead with range_ahead = msg.ranges[len(msg.ranges)/2] . Alternatively we could use msg.range_min to use the minimum value the laser scanner is capable of sensing (assuming our driver has properly set this). How would these choices affect the TurtleBot's behavior.
-        rospy.loginfo("range ahead: %0.1f" % range_ahead)
-        if self.driving_forward: #Set the command velocities for moving forward at 0.2 m/s
-            self.twist.linear.x = 0.2
-            self.twist.angular.z = 0
-            rospy.loginfo('Moving')
-            if range_ahead < 1.2 or rospy.Time.now() > self.state_change_time: #If there's no obstacle within 1.2m or the robot has been moving for 10 seconds, stop moving.
-                self.driving_forward = False
-                self.state_change_time = rospy.Time.now() + rospy.Duration(5) #Stop moving for 5 seconds.
-        else:
-            self.twist.angular.z = 1 #Spin on the spot for 5 seconds
-            self.twist.linear.x = 0
-            rospy.loginfo('Spinning')
-            if rospy.Time.now() > self.state_change_time: #Check that 5 seconds have elapsed
-                self.driving_forward = True
-                rospy.loginfo('Switch')
-                self.state_change_time = rospy.Time.now() + rospy.Duration(10) #Change states after 10 seconds
+g_range_ahead = 1 # anything to start
+scan_sub = rospy.Subscriber('scan', LaserScan, scan_callback)
+cmd_vel_pub = rospy.Publisher('cmd_vel', Twist, queue_size=1)    
+rospy.init_node('wander')
+state_change_time = rospy.Time.now()
+driving_forward = True 
+rate = rospy.Rate(10)
 
+while not rospy.is_shutdown():
+    if driving_forward:
+        if (g_range_ahead < 0.8 or rospy.Time.now() > state_change_time: #If there's no obstacle within 0.8m or the robot has been moving for 10 seconds, stop moving.
+            driving_forward = False
+            state_change_time = rospy.Time.now() + rospy.Duration(5) #Stop moving for 5 seconds.
+    else: # we're not driving_forward
+        if rospy.Time.now() > state_change_time: #Check that 5 seconds have elapsed
+            driving_forward = True # we're done spinning, time to move forward
+            state_change_time = rospy.Time.now() + rospy.Duration(30)
+    twist = Twist()
+    if driving_forward:
+            twist.linear.x = 1
+    else:
+        twist.angular.z = 1
+    cmd_vel_pub.publish(twist)
 
-        self.cmd_vel_pub.publish(self.twist) #Publish the messages
-        self.rate.sleep()
-
-def main():
-    wandering = Wandering()
-    rospy.Subscriber('/scan', LaserScan, wandering.scan_callback)
-    try:
-        rospy.spin()
-    except KeyboardInterrupt:
-        print("Shutting down")
-
-if __name__=='__main__':
-    main()
+    rate.sleep()
